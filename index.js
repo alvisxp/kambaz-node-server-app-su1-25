eb71ef2317f5cc410449185cc06ebcaa751f6ed0
alvisxp <128601501+alvisxp@users.noreply.github.com>
2025-06-06 15:00:58 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import Lab5 from "./Lab5/index.js";
+// import Hello from "./Hello.js"
+import cors from 'cors';
+const app = express();
+// Hello(app);
+
+app.use(cors());
+app.use(express.json());
+Lab5(app);
+app.listen(process.env.PORT || 4000)
